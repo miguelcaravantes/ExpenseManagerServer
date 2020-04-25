dc0c996086c49ab744a1d92ba5e72546ba09a0df
Miguel Caravantes <miguel.caravantes0@gmail.com>
2020-04-24 19:11:21 -0500

    initial commit

---
+++ ExpenseManager/Controllers/ExpensesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ExpenseManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpensesController : ControllerBase
+    {
+        
+        private readonly ILogger<ExpensesController> _logger;
+
+        public ExpensesController(ILogger<ExpensesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
